d0a2687ed2c193362c9f56dc24fbcad33632d9a4
Максим <makc-dmv@yandex.ru>
2020-09-17 20:10:24 +0500

    доделал форму написать нам

--- src/js/main.js
+++ src/js/main.js
@@ -1,99 +1,26 @@
 import svg4everybody from 'svg4everybody';
 // import './components/slick-init'
 // import './components/components';
-
-svg4everybody();
+import {
+  handleClickEmail,
+  handleClickMenu,
+  handleClickSearch
+} from './components/modals';
 
 const email = document.querySelector('.btn_email');
 const humburger = document.querySelector('.btn_menu');
 const search = document.querySelector('.btn_search');
-/**
- * Клик по кнопке почты.
- */
-const handleClickEmail = (evt) => {
-  const writeUs = document.querySelector('.write-us');
-  const input = writeUs.querySelector('input');
+const formWriteUs = document.querySelector('.write-us');
 
-  if (writeUs) {
-    const close = writeUs.querySelector('.write-us__btn-close');
-    const closeText = writeUs.querySelector('.write-us__btn-close-text');
-
-    const handleClickClose = (evt) => {
-      const { type, code } = evt;
-      /**
-       * закрываем форму и удаляем события
-       */
-      const closeForm = () => {
-        writeUs.classList.remove('write-us_open-js');
-        close.removeEventListener('click', handleClickClose);
-        closeText.removeEventListener('click', handleClickClose);
-        window.removeEventListener('keydown', handleClickClose);
-      };
-
-      if (type === 'keydown' && code === 'Escape') {
-        closeForm();
-      } else {
-        closeForm();
-      }
-    };
-
-    writeUs.classList.add('write-us_open-js');
-    input.focus();
-
-    if (close && closeText) {
-      close.addEventListener('click', handleClickClose);
-      closeText.addEventListener('click', handleClickClose);
-      window.addEventListener('keydown', handleClickClose);
-    }
-  }
-};
-/**
- * Клик по кнопке меню на мобилке.
- */
-const handleClickMenu = () => {
-  const nav = document.querySelector('.header__nav');
-
-  if (nav) {
-    const close = nav.querySelector('.btn_close-modal');
-
-    const handleClickCloseNav = () => {
-      nav.classList.remove('header__nav_open-js');
-      close.removeEventListener('click', handleClickCloseNav);
-    };
-
-    nav.classList.add('header__nav_open-js');
-
-    if (close) {
-      close.addEventListener('click', handleClickCloseNav);
-    }
-  }
-};
-/**
- * Клик по кнопке поиска.
- */
-const handleClickSearch = () => {
-  const formSearch = document.querySelector('.header__form-search');
-  const inputSearch = formSearch.querySelector('.header__form-input-search');
-
-  if (formSearch) {
-    const close = formSearch.querySelector('.btn_close-modal');
-
-    const handleClickCloseNav = () => {
-      formSearch.classList.remove('header__form-search_open-js');
-      close.removeEventListener('click', handleClickCloseNav);
-    };
-
-    formSearch.classList.add('header__form-search_open-js');
-    inputSearch.focus();
-
-    if (close) {
-      close.addEventListener('click', handleClickCloseNav);
-    }
-  }
-};
+svg4everybody();
 
 if (email) {
   email.addEventListener('click', handleClickEmail);
+  formWriteUs.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const formData = new FormData(evt.target);
+    alert(formData.get('tel'));
+  });
 }
 
 if (humburger) {
