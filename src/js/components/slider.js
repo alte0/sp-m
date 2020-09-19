0c0783d3a6b908d5e2affab64a1679daee1ed4a0
Максим <makc-dmv@yandex.ru>
2020-09-19 18:41:20 +0500

    доделал слайдер

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -8,18 +8,8 @@ export const slider = () => {
     },
     wrapperClass: 'slider-main__wrap',
     slideClass: 'slider-main__item',
-    // setWrapperSize: true,
-    // autoHeight: true,
-    // Optional parameters
-    // direction: 'vertical',
     loop: true,
     simulateTouch: false,
-
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-
     // Navigation arrows
     navigation: {
       nextEl: '.slider-main__btn-prev',
@@ -27,20 +17,55 @@ export const slider = () => {
     }
   };
   const sliderContainer = document.querySelector('.slider-main');
+  const progressBar = document.querySelector('.slider-main__progress-bar');
   const mySwiper = new Swiper(sliderContainer, configSwiper);
+  const TIME_OUT = configSwiper.autoplay.delay / 100;
+  let percent = 0;
+  let timerID = null;
+  /**
+   * Устанавливает ширину бара.
+   * @param percent
+   */
+  const setWidthBar = (percent) => {
+    progressBar.setAttribute('style', `transform: translateX(${-100 + percent}%);`);
+  };
+  /**
+   * Обновляет ширину бара до 100% ширины.
+   */
+  const setWidthUpdateBar = () => {
+    percent += 1;
 
-  const handleMouseenterAndMouseleaveEvents = (evt) => {
-    const { type } = evt;
+    if (percent >= 100) {
+      percent = 0;
+      setWidthBar(percent);
+      clearTimeout(timerID);
+    }
 
+    setWidthBar(percent);
+  };
+
+  timerID = setInterval(setWidthUpdateBar, TIME_OUT);
+
+  const handleMouseenterAndMouseleaveEvents = ({ type }) => {
     if (type === 'mouseenter') {
       mySwiper.autoplay.stop();
+      percent = 0;
+      clearTimeout(timerID);
+      setWidthBar(percent);
     } else if (type === 'mouseleave') {
       mySwiper.autoplay.start();
+      timerID = setInterval(setWidthUpdateBar, TIME_OUT);
     }
   };
 
   sliderContainer.addEventListener('mouseenter', handleMouseenterAndMouseleaveEvents);
   sliderContainer.addEventListener('mouseleave', handleMouseenterAndMouseleaveEvents);
+  mySwiper.on('slideChange', function () {
+    clearTimeout(timerID);
+    percent = 0;
 
-  console.log('slider');
+    if (mySwiper.autoplay.running) {
+      timerID = setInterval(setWidthUpdateBar, TIME_OUT);
+    }
+  });
 };
